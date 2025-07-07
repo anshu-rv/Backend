7c8499521761458f2acf4d28a2c3c1de4090d454
anshu-rv <anshuanu756@gmail.com>
2025-07-07 13:50:03 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter the number:");
+            int number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"the number:{number} is even.");
+
+            }
+            else
+            {
+                Console.WriteLine($"the number:{number} is odd.");
+            }
+
+
+
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
