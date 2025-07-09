74e825a0dab825cedaca8ed5b6aeb7594dd437c1
anshu-rv <anshuanu756@gmail.com>
2025-07-09 17:17:53 +0530

    Add files via upload

---
+++ Animal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Animal
+    {
+        public virtual void Animalsound()
+        {
+            Console.WriteLine("Animal is making sound");
+        }
+    }
+    class Dogs : Animal
+    {
+        public override void Animalsound()
+        {
+            Console.WriteLine("Dog is barking");
+        }
+
+    }
+    class Cats : Animal
+    {
+        public override void Animalsound()
+        {
+            Console.WriteLine("Cat is meowing");
+        }
+    }
+}
