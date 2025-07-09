a00883156c5b8374ce439ea4a5fc9e5deeb7167a
anshu-rv <anshuanu756@gmail.com>
2025-07-09 17:19:05 +0530

    Add files via upload

---
+++ Class3.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace ConsoleApp1
+{
+    interface IFirstInterface
+    {
+        public void MyMethod();
+    }
+    interface ISecondInterface
+    {
+        public void MyOtherMethod();
+    }
+    class DemoClass :IFirstInterface,ISecondInterface
+    {
+        public void MyMethod()
+        {
+            Console.WriteLine("This is the first method from the first interface.");
+        }
+        public void MyOtherMethod()
+        {
+            Console.WriteLine("This is second interface");
+        }
+    }
+    
+}
+
