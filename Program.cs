b1e6e814e1af41926905306a1c0bf01dd24559f0
anshu-rv <anshuanu756@gmail.com>
2025-07-09 17:20:05 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,26 +4,64 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main()
+        
+        static void Main(string[] args)
         {
-            Console.Write("Enter the number:");
-            int number = int.Parse(Console.ReadLine());
-            if (number % 2 == 0)
-            {
-                Console.WriteLine($"the number:{number} is even.");
+            
 
-            }
-            else
-            {
-                Console.WriteLine($"the number:{number} is odd.");
-            }
+            Car c = new Car();
+            Car Ford = new Car();
+            Bike b = new Bike();
+            Stud s = new Stud("anshu",21,91);
+            Dog d = new Dog();
+            d.Name = "xyx";
+            Mynewcar mycar = new Mynewcar();
+            mycar.hook();
+        
+            Console.WriteLine(mycar.carname+" "+mycar.name);
+            Animal animal = new Animal();
+            Animal cats = new Cats();
+            Animal dogs = new Dogs();
+            animal.Animalsound();
+            cats.Animalsound();
+            dogs.Animalsound();
+            Cycle cycle = new Cycle();
+            cycle.sleep();
+            cycle.Display();
+            Pig pig = new Pig();
+            pig.Animalsvoice();
+            DemoClass demo = new DemoClass();
+            demo.MyMethod();
+            demo.MyOtherMethod();
 
+            Console.WriteLine(d.Name);
 
-
+            Console.WriteLine(s.name + " " + s.id + " " + s.age);
+            Console.WriteLine(c.color);
+            Console.WriteLine(c.speed);
+            Console.WriteLine(b.color);
+            Console.WriteLine(b.name);
+            Console.WriteLine(Ford.name = "Ford");
+            Console.WriteLine(Ford.model = "Mustang");
+            c.Display();
+            
         }
+        //{
+        //    Console.WriteLine("Hello" + name );
+        //    Console.WriteLine("You are" + age +"years old");
+        //    Console.ReadLine();
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Fun("Anshu",21);
+        //    Fun("Bhaskar", 20);
+        //    Fun("Deepthi", 20);
+        //}
+        }
+       }
 
-    }
-}
+
+
 
 
 
