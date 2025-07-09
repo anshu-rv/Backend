ee411f99d515ac46b06b3830da5e9032c444f4bd
anshu-rv <anshuanu756@gmail.com>
2025-07-09 17:21:17 +0530

    Add files via upload

---
+++ Veachicals.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    abstract class Veachicals
+    {
+        public abstract void Display();
+        public void sleep()
+        {
+            Console.WriteLine("sleep zz...");
+
+        } 
+    }
+    class Cycle:Veachicals
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Cycle is running");
+        }
+    }
+}
