f3e53d269990ddf0b048115c524ae118f4cc44b2
anshu-rv <anshuanu756@gmail.com>
2025-07-09 17:18:25 +0530

    Add files via upload

---
+++ Animals.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1;
+
+namespace ConsoleApp1
+{
+    interface IAnimals
+    {
+        public void Animalsvoice(); 
+    }
+}
+class Pig : IAnimals
+{
+    public void Animalsvoice()
+    {
+        Console.WriteLine("pig is grunting");
+    }
+}
