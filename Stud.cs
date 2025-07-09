2b44f4684b060ec327565d513d54813a04409034
anshu-rv <anshuanu756@gmail.com>
2025-07-09 17:21:03 +0530

    Add files via upload

---
+++ Stud.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Stud
+    {
+        public string name;
+        public int age;
+        public int id;
+
+        public Stud(string Myname,int Myage, int Myid)
+        {
+            name = Myname;
+            age = Myage;
+            id = Myid;
+        }
+    }
+}
