314ef4b2263a9d67d107a8abe29b219335bb557d
anshu-rv <anshuanu756@gmail.com>
2025-07-09 17:19:45 +0530

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   internal class Car
+
+    {
+        public string color = "red";
+        public double speed = 100.7;
+        public string name;
+        public string model;
+        public void Display()
+        {
+            Console.WriteLine("Car Name: " + color);
+           
+        }
+    }
+}
