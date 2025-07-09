4225b9734feedaa5b95f32ca987ebfde492ec5ac
anshu-rv <anshuanu756@gmail.com>
2025-07-09 17:18:40 +0530

    Add files via upload

---
+++ Bike.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Bike
+    {
+        public string color;
+        public string name;
+
+
+        public Bike()
+        {
+            color = "black";
+            name = "Gt";
+        }
+    }
+}
+  
