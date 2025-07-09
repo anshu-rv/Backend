55ce60238ccffc690967ac467dafb2dd5ad6a360
anshu-rv <anshuanu756@gmail.com>
2025-07-09 17:20:41 +0530

    Add files via upload

---
+++ Mycar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal /*sealed*/ class Mycar
+    {
+        public string name = "bmw";
+        public void hook()
+        {
+            Console.WriteLine("bmw car");
+        }
+    }
+    class Mynewcar:Mycar
+    {
+        public string carname="audi";
+    }
+}
